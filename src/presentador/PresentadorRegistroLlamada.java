5afca9b49298049763ad5b4d996a67ab8426a363
Ian Lehmann <Ian Lehmann@DESKTOP-F0CEIPU>
2020-04-22 17:44:14 -0300

    agregado nivel, cantidad de sintomas y que no se repita los sintomas

--- src/presentador/PresentadorRegistroLlamada.java
+++ src/presentador/PresentadorRegistroLlamada.java
@@ -28,9 +28,9 @@ public class PresentadorRegistroLlamada {
         this.persin = new PersistenciaSintomas();
     }
     
-    public void agregarPesonas(int dni, String nya, String domicilio, int telefono){
+    public void agregarPesonas(int dni, String nya, String domicilio, int telefono, int lvl){
         try{
-            Persona np = new Persona(dni, nya, domicilio, telefono);
+            Persona np = new Persona(dni, nya, domicilio, telefono, lvl);
             this.perper.agregarPersona(np);
             vista.notificarPersonaAgregada();
         }catch(Exception e){
@@ -40,19 +40,31 @@ public class PresentadorRegistroLlamada {
     }
     
     public void agregarSintoma(String sin){
+        int lvl = 0;
         try{
-            Sintoma ns = new Sintoma(sin);
-            this.persin.agregarSintoma(ns);
+            Sintoma s = new Sintoma(sin);
+            
+            if(s.getDescripcion() == "Dificultad para respirar"){
+                lvl = 3;
+            }
+            else if(s.getDescripcion() == "Fiebre" || s.getDescripcion() == "Nauseas o vomitos" ||
+                    s.getDescripcion() == "Dolor de cabeza" || s.getDescripcion() == "Escalofrios"){
+                lvl = 2;
+            }
+            else{
+                lvl = 1;
+            }
+            this.persin.agregarSintoma(s);
         }
         catch(Exception e){
             JOptionPane.showMessageDialog(null, "Por favor, agrege uno o varios sintomas");
         }
         finally{
-            this.mostrarSintomas();
+            this.mostrarSintomas(lvl);
         }
     }
     
-    public void mostrarSintomas(){
-        vista.mostrarSintomas(persin.getSintomas());
+    public void mostrarSintomas(int lvl){
+        vista.mostrarSintomas(persin.getSintomas(), lvl);
     }
 }
